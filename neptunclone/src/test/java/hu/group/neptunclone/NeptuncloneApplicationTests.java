6211d8f028ad1254cb9ad8c2e619efca41c9e128
Ákos Balogh <akosbalogh16@gmail.com>
2022-10-20 00:12:30 +0200

    first commit

---
+++ neptunclone/src/test/java/hu/group/neptunclone/NeptuncloneApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.group.neptunclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeptuncloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
