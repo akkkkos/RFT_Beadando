2883ac41ced71021c4c8021b11af209f29e74703
Ákos Balogh <akosbalogh16@gmail.com>
2022-11-08 22:24:07 +0100

    Hotfix

---
+++ neptunclone/src/main/java/hu/rftbeadando/neptunclone/NeptuncloneApplication.java
@@ -0,0 +1,13 @@
+package hu.rftbeadando.neptunclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeptuncloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeptuncloneApplication.class, args);
+	}
+
+}
---
+++ neptunclone/src/test/java/hu/rftbeadando/neptunclone/NeptuncloneApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.rftbeadando.neptunclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeptuncloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
